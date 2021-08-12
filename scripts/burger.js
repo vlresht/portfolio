0ccb5bed34de330dfe4609bbd9a05b9d43a77c20
vlresht <vlreshtanenko@gmail.com>
2021-08-12 11:31:09 +0300

    small fixes to burger menu

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,4 +1,5 @@
 const burgerButton = document.querySelector('.menu__icon');
+
 if (burgerButton) {
   const menuBody = document.querySelector('.menu__body');
   burgerButton.addEventListener('click', function(e) {
@@ -7,3 +8,14 @@ if (burgerButton) {
     menuBody.classList.toggle('menu__body--active');
   })
 }
+
+const burgerLinks = document.querySelectorAll('.menu__link');
+
+burgerLinks.forEach((link) => {
+  const menuBody = document.querySelector('.menu__body');
+  link.addEventListener('click', function(e) {
+    document.body.classList.toggle('locked');
+    burgerButton.classList.toggle('menu__icon--active');
+    menuBody.classList.toggle('menu__body--active');
+  })
+})
