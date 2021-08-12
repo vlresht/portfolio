c84044beee933a34719c27fe578b5db13633487d
vlresht <vlreshtanenko@gmail.com>
2021-08-12 19:16:54 +0300

    final commit

--- scripts/slider.js
+++ scripts/slider.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
   $('.slider').slick({
     arrows: false,
     dots: true,
-    adaptiveHeight: true,
     slidesToShow: 2,
     slidesToScroll: 2,
     responsive: [
