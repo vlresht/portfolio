ecbd9bf31bd043595d753ae8c0447180c68c3579
vlresht <vlreshtanenko@gmail.com>
2021-08-12 12:10:37 +0300

    remove heart for now

--- scripts/heart.js
+++ scripts/heart.js
@@ -1,6 +1,6 @@
-const heart = document.querySelector('.heart');
+/*const heart = document.querySelector('.heart');
 if (heart) {
   let beatTimer = setInterval(() => {
     heart.classList.toggle('heart--small');  
   }, 500);
-}
+}*/
